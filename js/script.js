d2cf34fbbf3cf3d9cb4345850fc3a960655e6b58
Andres Cayambe <andrescayambe99@hotmail.com>
2023-03-29 14:29:21 +0200

    [end-fizzbuzz]

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.container');
+const limit = 100;
+let i;
+
+for (let i = 1; i <= limit; i++) {
+
+  const box = document.createElement('div');
+
+  box.className = 'box'; 
+
+  if (!(i % 15)) {
+    console.log('FizzBuzz');
+    box.innerHTML = ('FizzBuzz');
+    box.classList.add('red');
+  } else if (!(i % 3)) {
+    console.log('Fizz');
+    box.innerHTML = ('Fizz');
+    box.classList.add('green');
+  } else if (!(i % 5)) {
+    console.log('Buzz');
+    box.innerHTML = ('Buzz');
+    box.classList.add('yellow');
+  }  else {
+    console.log(i);
+    box.innerHTML = i;
+  }
+  container.append(box);
+}
